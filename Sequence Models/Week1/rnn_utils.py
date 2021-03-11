3b84b3d706b49dc938ba7596bc16d2ab1a0298e6
Jeffrey JALAL <Jeffrey.Jalal@gmail.com>
2021-03-11 15:08:21 +0100

    Sequence Models, week1 - Dinosaurus Island, character level language model : done

--- Sequence Models/Week1/rnn_utils.py
+++ Sequence Models/Week1/rnn_utils.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def return_zero():
+    return 0
+
 def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
@@ -45,6 +48,12 @@ def initialize_adam(parameters) :
     return v, s
 
 
+def update_parameters(parameters, gradient, learning_rate = 0.01):
+    
+    return None
+    
+
+
 def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate = 0.01,
                                 beta1 = 0.9, beta2 = 0.999,  epsilon = 1e-8):
     """
@@ -76,33 +85,23 @@ def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate = 0.01
     # Perform Adam update on all parameters
     for l in range(L):
         # Moving average of the gradients. Inputs: "v, grads, beta1". Output: "v".
-        ### START CODE HERE ### (approx. 2 lines)
         v["dW" + str(l+1)] = beta1 * v["dW" + str(l+1)] + (1 - beta1) * grads["dW" + str(l+1)] 
         v["db" + str(l+1)] = beta1 * v["db" + str(l+1)] + (1 - beta1) * grads["db" + str(l+1)] 
-        ### END CODE HERE ###
 
         # Compute bias-corrected first moment estimate. Inputs: "v, beta1, t". Output: "v_corrected".
-        ### START CODE HERE ### (approx. 2 lines)
         v_corrected["dW" + str(l+1)] = v["dW" + str(l+1)] / (1 - beta1**t)
         v_corrected["db" + str(l+1)] = v["db" + str(l+1)] / (1 - beta1**t)
-        ### END CODE HERE ###
 
         # Moving average of the squared gradients. Inputs: "s, grads, beta2". Output: "s".
-        ### START CODE HERE ### (approx. 2 lines)
         s["dW" + str(l+1)] = beta2 * s["dW" + str(l+1)] + (1 - beta2) * (grads["dW" + str(l+1)] ** 2)
         s["db" + str(l+1)] = beta2 * s["db" + str(l+1)] + (1 - beta2) * (grads["db" + str(l+1)] ** 2)
-        ### END CODE HERE ###
 
         # Compute bias-corrected second raw moment estimate. Inputs: "s, beta2, t". Output: "s_corrected".
-        ### START CODE HERE ### (approx. 2 lines)
         s_corrected["dW" + str(l+1)] = s["dW" + str(l+1)] / (1 - beta2 ** t)
         s_corrected["db" + str(l+1)] = s["db" + str(l+1)] / (1 - beta2 ** t)
-        ### END CODE HERE ###
 
         # Update parameters. Inputs: "parameters, learning_rate, v_corrected, s_corrected, epsilon". Output: "parameters".
-        ### START CODE HERE ### (approx. 2 lines)
         parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * v_corrected["dW" + str(l+1)] / np.sqrt(s_corrected["dW" + str(l+1)] + epsilon)
         parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * v_corrected["db" + str(l+1)] / np.sqrt(s_corrected["db" + str(l+1)] + epsilon)
-        ### END CODE HERE ###
 
     return parameters, v, s
